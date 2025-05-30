d07585cefd77004369b05fbdc9e317e3347e44ba
arshelton <135093296+arshelton@users.noreply.github.com>
2025-05-29 21:39:20 -0500

    Fix config base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://arshelton.github.io/factorio-cbplanner-react/",
+  base: "/factorio-cbplanner-react/",
   plugins: [tailwindcss(), react()],
 });
